06484e45fc3a5691f36c9cdc6a34c96dbb75e240
hazkerindii228 <59193313+PavelGem-13g@users.noreply.github.com>
2023-10-23 20:22:42 +0300

    add tableview with data

--- classwork/classwork/ViewController.swift
+++ classwork/classwork/ViewController.swift
@@ -7,33 +7,57 @@
 
 import UIKit
 
-final class ViewController: UIViewController {
+final class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        beerData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let beer = beerData[indexPath.row]
+        var cell = UITableViewCell()
+        var configuration = cell.defaultContentConfiguration()
+//        configuration.image = UIImage
+        configuration.text = beer.name
+        configuration.secondaryText = beer.tagline
+        cell.contentConfiguration = configuration
+        return cell
+    }
+    
+    
+    private lazy var tableView:UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .cyan
+        tableView.dataSource = self
+        return tableView
+    }()
+
+    private var beerData: [BeerDTO] = [];
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
         
+        tableView.translatesAutoresizingMaskIntoConstraints = false;
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+        
+        
         let url:URL = URL(string: "https://api.punkapi.com/v2/beers")!
         URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
-            guard
-                let data,
-                let response,
-                error == nil
-            else{
-                return
-            }
-            //let str = String(data:data, encoding: .utf8)
-            //print ("Полученные данные: \(str ?? "")")
+            guard let data = data, error == nil else { return }
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
-            //
-            let model = try! decoder.decode([BeerDTO].self, from: data)
-            print(model)
+            self.beerData = try! decoder.decode([BeerDTO].self, from: data)
+            DispatchQueue.main.async(execute: {
+                self.tableView.reloadData()
+            })
         }).resume()
-        
     }
-
-
 }
 struct BeerDTO :Decodable {
     let id: Int
